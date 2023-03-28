0ede05da06e10322e81efa1cf287b31b52e2eaf2
painter98 <120320713+painter98@users.noreply.github.com>
2023-03-28 20:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let header = document.getElementsById("url").value;
 let name = document.getElementsById("name").value;
 let year = document.getElementsById("year").value;
 
+if(name != '' && year != ''){
+	header.textContent = header + '/?name=' + name + '&year=' + year;  
+}
+else if(name == '' && year != ''){
+	header.textContent = header + '/?year=' + year;
+}
+else{
+	header.textContent = header + '/?name=' + name;
+}
