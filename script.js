f6740d4182caed5a0489513268b12b9e115bce26
Pallavi <120320713+painter98@users.noreply.github.com>
2023-05-23 14:00:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-  function extend(){
+  document.getElementById('button').addEventListener('click',()=>{
+    console.log('hii');
             let header = document.getElementById("url").textContent;
             let name = document.getElementById("name").value;
             let year = document.getElementById("year").value;
             let concat = header;
            
         concat=operation(name,year,header,concat);
-		document.querySelector("#url").innerHTML = concat;
-        }
+		document.querySelector("#url").innerText = concat;
+    console.log(document.getElementById('url').innerText);
+  })
+        
        function operation(name,year,header,concat){
 	        if(name != '' && year != ''){
                concat = concat + "?name=" + name + "&year=" + year; 
